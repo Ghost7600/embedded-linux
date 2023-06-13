09dd72968d63c1ef5f26e16f7966a313ee99ad68
Ghost7600 <kenner.trabalhos@gmail.com>
2023-06-13 11:54:52 -0300

    initial commit

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+podman run -it --rm --security-opt label=disable --userns=keep-id:uid=1000,gid=1000 --volume /var/data/aluno/lxe22109/:/home/aluno/lxe22109 docker.io/hmarcondes/dev_space:latest
